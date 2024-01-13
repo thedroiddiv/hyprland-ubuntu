f7a423aa1bccddb6700d732a7eb4b60b27b41349
Divyansh Kushwaha <thedroiddiv@gmail.com>
2024-01-13 13:02:56 +0530

    Update build-hyprland-ubuntu-23.10.sh

--- build-hyprland-ubuntu-23.10.sh
+++ build-hyprland-ubuntu-23.10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-build-from-nothing () {
+install-dependencies () {
 
 sudo apt-get install -y nala
 sudo nala install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev libpango1.0-dev xdg-desktop-portal-wlr hwdata libwlroots-dev libliftoff-dev libinput-tools
@@ -99,6 +99,8 @@ meson setup --prefix=/usr --buildtype=release
 ninja
 sudo ninja install
 
+cd ../..
+
 }
 
 ## Building Hyprland
@@ -128,6 +130,6 @@ then
   cd HyprSource
   build-hyprland
 else 
-  build-from-nothing
+  install-dependencies
   build-hyprland
 fi
